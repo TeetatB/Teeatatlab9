02e6c059620e2849ec407c2d45096f2542b31003
TeetatB <teetatmix36108@gmail.com>
2023-12-15 16:11:10 +0700

    lab9_1 completed 4sho

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
-double pb,ir,p;
-int n=1;
+
 int main(){	
+    double pb,ir,p,nb,t,i;
+    int n=1;
 	cout << "Enter initial loan: ";
 	cin >> pb;
 	cout << "Enter interest rate per year (%): ";
@@ -21,28 +22,20 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	double i = ir*pb/100;
-	double t = pb+i;
-	double nb = t-p;
 	while(nb > 0){
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << n++; //endofyear
-	cout << setw(13) << left << pb; //previous balance
+	cout << setw(13) << left << n++; 
+	cout << setw(13) << left << pb; 
 	i = ir*pb/100;
-	cout << setw(13) << left << i; //interest
+	cout << setw(13) << left << i; 
 	t = pb+i;
-	cout << setw(13) << left << t; //total
-	if (t < p){
-		p = t;
-	}
-	cout << setw(13) << left << p; //payment
+	cout << setw(13) << left << t; 
+	if (t < p) p = t;
+	cout << setw(13) << left << p; 
 	nb = t-p;
-	cout << setw(13) << left << nb; //newbalance
-	pb = nb; //change previous balance
-	cout << "\n";	
+	cout << setw(13) << left << nb; 
+	pb = nb; 
+	cout << "\n";
 	}
 	return 0;
 }
